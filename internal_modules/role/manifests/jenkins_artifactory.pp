84638512a087e43e2ca6fb22cda75cb3345276cb
mahbubrahman <mahbub.rahman@gmail.com>
2014-01-26 23:02:40 -0500

    Update jenkins_artifactory.pp

--- internal_modules/role/manifests/jenkins_artifactory.pp
+++ internal_modules/role/manifests/jenkins_artifactory.pp
@@ -2,9 +2,9 @@ class role::jenkins_artifactory {
   include profile::jenkins
   include profile::artifactory
   
-  class { '::epel': } ->
-  class { '::avahi':
-    firewall => true
-  }  
+  # class { '::epel': } ->
+  # class { '::avahi':
+  #  firewall => true
+  # }  
   
 }
